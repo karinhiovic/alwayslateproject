ccb70209fb97ccc28d453cbaab9501010ada3c5c
karinhiovic <karinobergs99@gmail.com>
2023-11-30 22:52:43 +0100

    Init of app

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'alwayslate';
 }
